99781e8d420b150cdcec468259b466807d46186f
ericwilliammarshall <eric.marshall@mac.com>
2018-02-17 22:27:11 -0500

    sftp tweaks

--- sftp/manifests/init.pp
+++ sftp/manifests/init.pp
@@ -10,8 +10,12 @@ class sftp{
     }   
     file { '/etc/ssh/sshd_config':
         ensure => 'directory',
-        mode   => '0777',
-        source => 'puppet:///module/sftp/etc/ssh/sshd_config',
+        mode   => '0644',
+        source => 'puppet:///modules/sftp/etc/ssh/sshd_config',
+    }   
+    file { '/srv/sftp':
+        ensure => 'directory',
+        mode   => '0701',
     }   
     file { '/srv/sftp/upload':
         ensure => 'directory',
@@ -27,10 +31,36 @@ class sftp{
     user { 'dy78' :
         groups            => 'sftpuser',
     }
+    file { '/srv/sftp/dy78/upload':
+        ensure => 'directory',
+        mode   => '0701',
+    }   
+    file { '/srv/sftp/dy78/download':
+        ensure => 'directory',
+        mode   => '0704',
+    }   
+
     user { 'dyang' :
         groups            => 'sftpuser',
     }
+    file { '/srv/sftp/dyang/upload':
+        ensure => 'directory',
+        mode   => '0701',
+    }   
+    file { '/srv/sftp/dyang/download':
+        ensure => 'directory',
+        mode   => '0704',
+    }   
+
     user { 'ericmars' :
         groups            => 'sftpuser',
     }
+    file { '/srv/sftp/ericmars/upload':
+        ensure => 'directory',
+        mode   => '0701',
+    }   
+    file { '/srv/sftp/ericmars/download':
+        ensure => 'directory',
+        mode   => '0704',
+    }   
 }
