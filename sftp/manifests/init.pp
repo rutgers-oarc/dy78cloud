06d50ba3d6a9b086e1702718f3c6345197e00f39
ericwilliammarshall <eric.marshall@mac.com>
2018-02-18 15:08:35 -0500

    user tweaks

--- sftp/manifests/init.pp
+++ sftp/manifests/init.pp
@@ -28,39 +28,65 @@ class sftp{
     }
     # users
     # dependent on current setup 
+
+    # dy78
     user { 'dy78' :
-        groups            => 'sftpuser',
+        groups  => 'sftpuser',
+        before  => [Group['sftpuser'], 
     }
+    file { '/srv/sftp/dy78':
+        ensure => 'directory',
+        mode   => '0701',
+        before  => [Group['sftpuser'], File['/srv/sftp']],
+    }   
     file { '/srv/sftp/dy78/upload':
         ensure => 'directory',
         mode   => '0701',
+        before  => [Group['sftpuser'], File['/srv/sftp/dy78']],
     }   
     file { '/srv/sftp/dy78/download':
         ensure => 'directory',
         mode   => '0704',
+        before  => [Group['sftpuser'], File['/srv/sftp/dy78']],
     }   
 
+    # dyang
     user { 'dyang' :
-        groups            => 'sftpuser',
+        groups  => 'sftpuser',
     }
+    file { '/srv/sftp/dyang':
+        ensure => 'directory',
+        mode   => '0701',
+        before  => [Group['sftpuser'], File['/srv/sftp']],
+    }   
     file { '/srv/sftp/dyang/upload':
         ensure => 'directory',
         mode   => '0701',
+        before  => [Group['sftpuser'], File['/srv/sftp/dyang']],
     }   
     file { '/srv/sftp/dyang/download':
         ensure => 'directory',
         mode   => '0704',
+        before  => [Group['sftpuser'], File['/srv/sftp/dyang']],
     }   
 
+    # ericmars
     user { 'ericmars' :
-        groups            => 'sftpuser',
+        groups  => 'sftpuser',
     }
+    file { '/srv/sftp/ericmars':
+        ensure => 'directory',
+        mode   => '0701',
+        before  => [Group['sftpuser'], File['/srv/sftp']],
+    }   
     file { '/srv/sftp/ericmars/upload':
         ensure => 'directory',
         mode   => '0701',
+        before  => [Group['sftpuser'], File['/srv/sftp/ericmars']],
     }   
     file { '/srv/sftp/ericmars/download':
         ensure => 'directory',
         mode   => '0704',
+        before  => [Group['sftpuser'], File['/srv/sftp/ericmars']],
     }   
 }
